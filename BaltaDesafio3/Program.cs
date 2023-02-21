82035102c06eb8e324aca4196036e279a9a0c819
Felipe Pestana <pestana1984@gmail.com>
2023-02-21 12:00:31 -0300

    Add project files.

---
+++ BaltaDesafio3/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o valor da gasolina: ");
+        float gas = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o valor do álcool: ");
+        float alc = float.Parse(Console.ReadLine());
+        float rel = alc / gas;
+        if (rel <= 0.72)
+        {
+            Console.WriteLine("Use álcool.");
+        }
+        else
+        {
+            Console.WriteLine("Use gasolina.");
+        }
+    }
+}
